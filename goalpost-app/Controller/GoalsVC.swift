30b765eca429800f05809782f33cc5170499aed6
Andras <andras@MacBook.local>
2017-11-23 22:04:58 +1100

    End of Lesson 126

--- goalpost-app/Controller/GoalsVC.swift
+++ goalpost-app/Controller/GoalsVC.swift
@@ -16,6 +16,9 @@ class GoalsVC: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false
        
     }
 
@@ -23,3 +26,20 @@ class GoalsVC: UIViewController {
 
 }
 
+//Does not need to be an extension, just a preference as many things may go here
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else { return UITableViewCell() }
+        cell.configureCell(description: "Eat salad twice a week", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
+    
+}
---
+++ goalpost-app/Enums/GoalType.swift
@@ -0,0 +1,15 @@
+//
+//  GoalType.swift
+//  goalpost-app
+//
+//  Created by Andras on 23/11/17.
+//  Copyright © 2017 Andras. All rights reserved.
+//
+
+import Foundation
+
+enum GoalType: String  {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+    
+}
