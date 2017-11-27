2150369b1db0b8d7339edf0fce35a033ef1cb7af
Andras <andras@MacBook.local>
2017-11-27 21:32:29 +1100

    End of Lesson 131.

--- goalpost-app/Controller/FinishGoalVC.swift
+++ goalpost-app/Controller/FinishGoalVC.swift
@@ -7,6 +7,10 @@
 //
 
 import UIKit
+import CoreData
+
+//This can be used everywhere as it is outside of the class
+let appDelegate =  UIApplication.shared.delegate as? AppDelegate
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
 
@@ -26,11 +30,40 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         createGoalBtn.bindToKeyboard()
-        pointsTextField.delegate = self    }
+        pointsTextField.delegate = self
+        
+        
+    }
 
   
 
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismissDetail()
+    }
     @IBAction func createGoalBtnWasPressed(_ sender: Any) {
         //Pass data into Core Data
+        if pointsTextField.text != "" {
+        self.save { (complete) in
+            dismiss(animated: true, completion: nil)//dismiss to main viewcontroller
+            }
+        }
+     }
+    
+    func save(completion: (_ finished: Bool) ->()) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextField.text!)!
+        goal.goalProgress = Int32(0)
+        
+        do {
+        try managedContext.save()
+            completion(true)
+        } catch {
+            debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
     }
 }
